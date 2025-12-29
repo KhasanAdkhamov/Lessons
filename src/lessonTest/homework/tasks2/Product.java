eda5030ba4a5ea2fc7d44f68cf9c428ec928f053
hasan <hasan.adxamov@gmail.com>
2025-12-30 02:20:00 +0300

    задание 2 Product

---
+++ src/lessonTest/homework/tasks2/Product.java
@@ -0,0 +1,60 @@
+package lessonTest.homework.tasks2;
+
+public class Product {
+    private String name;
+    private double price;
+    private int quantity;
+
+    public Product(String name, double price, int quantity) {
+        setName(name);
+        setPrice(price);
+        setQuantity(quantity);
+    }
+
+    public double getTotalValue() {
+        return price * quantity;
+    }
+
+    public void sell(int amount) {
+        if(quantity != 0 && quantity >= amount) {
+            this.quantity -= amount;
+        }
+    }
+
+    public void restock(int amount) {
+        quantity += amount;
+    }
+
+    public void setName(String name) {
+        if(name.isEmpty()) {
+            throw new RuntimeException("name must not be empty");
+        }
+        this.name = name;
+    }
+
+    public void setPrice(double price) {
+        if(price < 0) {
+            throw new RuntimeException("price should be more 0");
+        }
+        this.price = price;
+    }
+
+    public void setQuantity(int quantity) {
+        if(quantity < 0) {
+            throw new RuntimeException("it should be positive amount, more 0");
+        }
+        this.quantity = quantity;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+}
