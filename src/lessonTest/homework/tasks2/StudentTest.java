0e808723d7f3511d73bed44346e1b15f5a984c42
hasan <hasan.adxamov@gmail.com>
2025-12-30 02:32:48 +0300

    задание 1 проверка на работу метода

---
+++ src/lessonTest/homework/tasks2/StudentTest.java
@@ -0,0 +1,8 @@
+package lessonTest.homework.tasks2;
+
+public class StudentTest {
+    public static void main(String[] args) {
+        Student student = new Student("Anna", 21, 3.0);
+        student.displayInfo();
+    }
+}
