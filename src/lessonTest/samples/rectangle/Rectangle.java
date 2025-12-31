f46194a99df174adc75e6115280617adda18b7fe
hasan <hasan.adxamov@gmail.com>
2026-01-01 00:00:07 +0300

    Rectangle.java выдает ошибку при создании экземпляра

---
+++ src/lessonTest/samples/rectangle/Rectangle.java
@@ -0,0 +1,63 @@
+package lessonTest.samples.rectangle;
+
+public class Rectangle {
+    private double width;
+    private double height;
+
+    public Rectangle(double width, double height) {
+        setWidth(width);
+        setHeight(height);
+    }
+
+    public Rectangle(double width) {
+        setWidth(width);
+        this.setHeight(getHeight());
+    }
+
+    public double getArea() {
+        return width * height;
+    }
+
+    public double getPerimeter() {
+        return 2 * (height + width);
+    }
+
+    public boolean isSquare() {
+        if(width != height) {
+            return false;
+        }
+        return true;
+    }
+
+    public void resize(double factor) {
+        System.out.println(getArea() * factor);
+    }
+
+    public double getWidth() {
+        if (width == 0) {
+            throw new RuntimeException("");
+        }
+        return width;
+    }
+
+    public void setWidth(double width) {
+        if(width == 0.0) {
+            throw new RuntimeException("it can not be 0");
+        }
+        this.width = width;
+    }
+
+    public double getHeight() {
+        if (height == 0) {
+            throw new NullPointerException("");
+        }
+        return height;
+    }
+
+    public void setHeight(double height) {
+        if(height == 0) {
+            throw new RuntimeException("it can not be 0");
+        }
+        this.height = height;
+    }
+}
