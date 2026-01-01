28443a144eff24d20323ea1fd70e871f6f82a7c8
hasan <hasan.adxamov@gmail.com>
2026-01-02 02:44:12 +0300

    Сделал сеттер с валидацией с использованием регулярного выражения

---
+++ src/lessonTest/samples/email/Email.java
@@ -0,0 +1,24 @@
+package lessonTest.samples.email;
+
+import java.util.regex.Pattern;
+
+public class Email {
+    private String email;
+    private String regex = "^\\w+@\\w+.\\w+.\\w+$";
+
+    public Email(String email) {
+        setEmail(email);
+    }
+
+    public void setEmail(String email) {
+        if (email.isEmpty() || Pattern.matches(regex, email)) {
+            throw new NullPointerException("enter you email");
+        }
+        this.email = email;
+    }
+
+
+    public String getEmail() {
+        return email;
+    }
+}
