4d1b34e3f3ff2edcaf1a46837272d28665d6cc46
hasan <hasan.adxamov@gmail.com>
2026-01-21 11:11:37 +0300

    java concepts Неизменяемый класс для email-адреса с валидацией в конструкторе. переопределил методы Objects

--- src/work_16_01_JAVA_CONCEPTS/hw/task2/Email.java
+++ src/work_16_01_JAVA_CONCEPTS/hw/task2/Email.java
@@ -1,5 +1,6 @@
 package work_16_01_JAVA_CONCEPTS.hw.task2;
 //Неизменяемый класс для email-адреса с валидацией в конструкторе.
+import java.util.Objects;
 import java.util.regex.Pattern;
 
 public final class Email {
@@ -20,5 +21,30 @@ public final class Email {
 
     public String getAddress() {
         return address;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Email: " +
+                "address %s", address);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null) return false;
+        if (this == o) return true;
+        if (getClass() != o.getClass()) return false;
+
+        Email other = (Email) o;
+
+        return Objects.equals(address, other.address);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = 19;
+
+        result = 7 * result + (address != null?address.hashCode():0);
+        return result;
     }
 }
--- src/work_16_01_JAVA_CONCEPTS/hw/task2/EmailTest.java
+++ src/work_16_01_JAVA_CONCEPTS/hw/task2/EmailTest.java
@@ -3,6 +3,8 @@ package work_16_01_JAVA_CONCEPTS.hw.task2;
 public class EmailTest {
     public static void main(String[] args) {
         Email email = new Email("hasan.adxamov@gmail.com");
-        System.out.println(email);
+        System.out.println(email.toString());
+        Email email2 = new Email("hasan.adxamov@gmail.com");
+        System.out.println(email.equals(email2));
     }
 }
