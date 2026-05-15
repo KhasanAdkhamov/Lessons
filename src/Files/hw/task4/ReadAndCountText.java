813c4130009dfb1041b7c8524df9dc18f0385e77
hasan <hasan.adxamov@gmail.com>
2026-05-15 15:58:37 +0300

    Files task4

--- src/Files/hw/task4/ReadAndCountText.java
+++ src/Files/hw/task4/ReadAndCountText.java
@@ -5,28 +5,24 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
 import java.nio.charset.StandardCharsets;
-
+// TODO убрать лишний try и цикл
 public class ReadAndCountText {
     public static void main(String[] args) throws IOException {
-        try (Reader reader = new FileReader("resources/text.poem", StandardCharsets.UTF_8)) {
-            int countChar = 0;
-            int x;
-            while ((x = reader.read()) != -1) {
-                countChar++;
-            }
-            System.out.println(countChar);
+//
             try (BufferedReader bufferedReader = new BufferedReader(new FileReader("resources/text.poem", StandardCharsets.UTF_8))) {
+                int countChar = 0;
                 int countLine = 0;
                 String line;
                 int wordCount = 0;
                 while ((line = bufferedReader.readLine()) != null) {
+                    countChar += line.length();
                     String[] split = line.trim().split("[\\p{Punct}\\s]+");
                     wordCount += split.length;
                     countLine++;
                 }
-                System.out.println(countLine);
-                System.out.println(wordCount);
+                System.out.println(countLine + " строки");
+                System.out.println(wordCount + " слов");
+                System.out.println(countChar + " символов");
             }
         }
     }
-}
